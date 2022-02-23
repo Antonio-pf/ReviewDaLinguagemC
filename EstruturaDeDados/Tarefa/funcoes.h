cb4af3eba67107273b97c975863e3754c03ba381
Antonio-pf <antoniopfelipe@gmail.com>
2022-02-23 09:37:47 -0300

    Criando função para a calculadora

--- EstruturaDeDados/Tarefa/funcoes.h
+++ EstruturaDeDados/Tarefa/funcoes.h
@@ -11,6 +11,7 @@
 
 // Implemente a função soma:
 int soma(int a, int b) {
+    cout << "\n\tOperacao  de adicao\n";
     // Seu código aqui:
 
 
@@ -19,6 +20,7 @@ int soma(int a, int b) {
 
 // Implemente a função subtracao:
 int subtracao(int a, int b) {
+    cout << "\n\tOperacao  de subtracao\n";
     // Seu código aqui:
 
 
@@ -27,6 +29,7 @@ int subtracao(int a, int b) {
 
 // Implemente a função multiplicacao:
 int multiplicacao(int a, int b) {
+   cout << "\n\tOperacao  de adicao\n";
     // Seu código aqui:
 
 
@@ -35,6 +38,7 @@ int multiplicacao(int a, int b) {
 
 // Implemente a função divisao:
 int divisao(int a, int b) {
+    cout << "\n\tOperacao  de adicao\n";
     // Seu código aqui:
 
 
@@ -42,7 +46,43 @@ int divisao(int a, int b) {
 }
 
 // Implemente a função calculadora:
-int calculadora(int a, int b, char operacao) {
+int calculadora(int valor1, int valor2, char operacao) {
+
+    do{
+        cout << "Escolha a operacao desejada\n";
+        cout << "\t1 - Adicao\n";
+        cout << "\t2 - Subtraçao\n";
+        cout << "\t3 - Divisao\n";
+        cout << "\t4 - Multiplicaçao\n";
+
+        cout << "Opcao: ";
+        cin >> op;
+
+    }while(op == 1);
+
+    if(op == 5){
+        cout << "Tchau!!!!!: \n";
+        return 0;
+    }
+
+    cout << "Digite o primeiro valor: ";
+    cin >> valor1;
+    cout << "Digite o segundo valor: ";
+    cin >> valor2;
+
+    switch(op)
+
+            case 1:
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            default:
+                    cout << "Operador invalido \: : \n"
+
     // Seu código aqui:
 
 
