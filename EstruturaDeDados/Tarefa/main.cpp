306bac16a2f930c0b0a89bbf1142318235389f55
Antonio-pf <antoniopfelipe@gmail.com>
2022-02-23 09:32:57 -0300

    Criando função calculadora

--- EstruturaDeDados/Tarefa/main.cpp
+++ EstruturaDeDados/Tarefa/main.cpp
@@ -10,45 +10,8 @@ using namespace std;
 
 
 
-int main() {
+int main(calculadora) {
 
-    float valor1, valor2;
-    char op;
-
-    do{
-        cout << "Escolha a operacao desejada\n";
-        cout << "\t1 - Adicao\n";
-        cout << "\t2 - Subtraçao\n";
-        cout << "\t3 - Divisao\n";
-        cout << "\t4 - Multiplicaçao\n";
-
-        cout << "Opcao: ";
-        cin >> op;
-
-    }while(op == 1);
-
-    if(op == 5){
-        cout << "Tchau!!!!!: \n";
-        return 0;
-    }
-
-    cout << "Digite o primeiro valor: ";
-    cin >> valor1;
-    cout << "Digite o segundo valor: ";
-    cin >> valor2;
-
-    switch(op)
-
-            case 1:
-            break;
-            case 2:
-            break;
-            case 3:
-            break;
-            case 4:
-            break;
-            default:
-                    cout << "Operador invalido \: : \n"
 
 }
 
